66ccd731b814ebb6479f7e742804bfacfd0fb9f5
Filip Havrlent <filip@havrlent.com>
2020-05-19 23:13:05 +0200

    Add agenda gui (#12)

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const {
+  AGENDA_PASSWORD,
+  AGENDA_USER,
   BOT_PASSWORD,
   BOT_USERNAME,
   CHANNEL_NAME,
@@ -8,10 +10,13 @@ const {
   CLIENT_SECRET,
   MONGO_DB,
   NODE_ENV,
+  PORT,
   SENTRY_DSN,
 } = process.env;
 
 export default {
+  agendaPassword: AGENDA_PASSWORD,
+  agendaUser: AGENDA_USER,
   botPassword: BOT_PASSWORD,
   botUsername: BOT_USERNAME,
   channelName: CHANNEL_NAME,
@@ -19,5 +24,6 @@ export default {
   clientSecret: CLIENT_SECRET,
   mongoDb: MONGO_DB,
   nodeEnv: NODE_ENV,
+  port: PORT,
   sentryDsn: SENTRY_DSN,
 };
